34ab3f46b159f84e0ddd53806fc6765861973196
Gabriel02Rech <96732165+Gabriel02Rech@users.noreply.github.com>
2022-01-27 17:44:52 -0300

    gabriel_rech

---
+++ atividade_1.js
@@ -0,0 +1,30 @@
+/*[ ] Toda a base de percentuais, serão de acordo com o salário e cargo atual;
+[ ] Adicionar o percentual fixo no atual salário de acordo com o cargo;
+[ ] A cada ano a mais de admissão é acrescido mais 3% no salário;
+[ ] Os calculos só deveram ser feitos para funcionários que ainda estão admitidos;
+let dados = require ('.dados.json');*/
+let obj = {
+    nome = dados.nome,
+    cargo = cargos.nome,
+    sal = cargos.salario,
+    ant_sal = cargos.salario,
+}
+    for(obj.cargo; cargos.atual=true; cargos.nome==obj.cargo);
+    for(obj.sal; cargos.atual=true; cargos.salario==obj.sal);
+    for(obj.ant_sal; cargos.atual=false; cargos.salario==obj.ant_sal);
+
+    while(dados._id11)
+    {
+        if(dados._id==dados._id){
+            if(dados.demitido=false){
+                console.log('O funcionário ${dados.nome} não teve seu salário alterado pois já consta como demitido.');
+                    }
+        }else
+        {
+            obj.sal=obj.saldados.percentual;
+        }
+        console.log('Seguem os dados atualizados do funcionário ${obj.nome}.');
+        console.log('Cargo ${obj.cargo}.');
+        console.log('Antigo salário ${obj.sal}.');
+        console.log('Novo salário ${obj.ant_sal}.');
+}   
